931d9cf9bda1608b0bab0f86db9e79fe40464b28
WillRegister <willregister777@gmail.com>
2025-07-29 13:42:33 -0500

    new file:   backend/main.py 	new file:   backend/requirements.txt 	new file:   frontend/.gitignore 	modified:   frontend/Dockerfile 	new file:   frontend/README.md 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/favicon.ico 	new file:   frontend/public/index.html 	new file:   frontend/public/logo192.png 	new file:   frontend/public/logo512.png 	new file:   frontend/public/manifest.json 	new file:   frontend/public/robots.txt 	new file:   frontend/src/App.css 	new file:   frontend/src/App.js 	new file:   frontend/src/App.test.js 	new file:   frontend/src/index.css 	new file:   frontend/src/index.js 	new file:   frontend/src/logo.svg 	new file:   frontend/src/reportWebVitals.js 	new file:   frontend/src/setupTests.js

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+
+# Connect to Redis (host must match your docker-compose service name)
+r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+
+@app.get("/ping-redis")
+def ping_redis():
+    try:
+        r.set("hello", "world")
+        value = r.get("hello")
+        return {"redis says": value}
+    except Exception as e:
+        return {"error": str(e)}

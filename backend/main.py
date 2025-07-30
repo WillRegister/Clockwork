9bff8056e97db08421d5627f53d2140695ce0282
WillRegister <willregister777@gmail.com>
2025-07-30 15:41:35 -0500

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,15 @@ import redis
 
 app = FastAPI()
 
+# Say Hello from root
+
+@app.get ("/")
+def read_root():
+    try:
+        return{"hello":"world"}
+    except Exception as e:
+        return {"error": str(e)}
+
 # Connect to Redis (host must match your docker-compose service name)
 r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
